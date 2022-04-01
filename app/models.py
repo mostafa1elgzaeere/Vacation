a372d0a42299f075e9152f38a9e587b60ba9d877
mostafa <mostafa>
2022-04-01 21:18:45 +0200

    htzbot

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from django.db.models import *
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractBaseUser,AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 
 class Vacation(Model):
-    employee=ForeignKey('CustomUser',on_delete=CASCADE,unique=True)
+    employee=OneToOneField('CustomUser',on_delete=CASCADE,unique=True)
     start_at=DateField()
     end_at=DateField()
     reason=CharField(max_length=200)
@@ -14,7 +14,7 @@ class Vacation(Model):
     
     
 class CustomUser(AbstractUser):
-    mobile_number=IntegerField(max_length=25)
+    mobile_number=IntegerField(null=True,blank=True)
     
     def __str__(self):
         return f"{self.username}"
