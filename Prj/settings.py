f9969bce25efee6e71edbde2eb835cd3128abd0c
mostafa <mostafa>
2022-04-01 16:22:03 +0200

    deployment test

--- Prj/settings.py
+++ Prj/settings.py
@@ -15,6 +15,8 @@ import os
 from threading import local
 import django
 import django_heroku
+from dj_database_url import parse as dburl
+from decouple import  config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,13 +101,16 @@ WSGI_APPLICATION = 'Prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+                'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
